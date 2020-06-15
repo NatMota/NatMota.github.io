e3ecce818dbbf23e67dd1b89cb55dfe544a7ab3c
Nat.Mota <nat.c.mota@gmail.com>
2020-06-15 11:10:54 +0100

    accessibility, styling, pwa,offline

---
+++ component---src-pages-404-js-ac8ab3faece17ba18dca.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),r=n.n(t),l=n("7oih"),c=n("Wbzz"),o=n("EYWl");a.default=function(){return r.a.createElement(l.a,null,r.a.createElement(o.a,{title:"Page not found"}),r.a.createElement("main",{className:"error-page"},r.a.createElement("div",{className:"error-container"},r.a.createElement("h1",null,"oops it's a dead end"),r.a.createElement(c.Link,{to:"/",className:"btn"},"back home"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ac8ab3faece17ba18dca.js.map
